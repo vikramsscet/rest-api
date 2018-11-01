ed86f4c710f42f5b470a3fa8aa90843d1598ba45
Rahul Pawar <rahul.pawar@globant.com>
2018-11-01 10:21:50 +0530

    hash correction

--- getPath.js
+++ getPath.js
@@ -1,4 +1,26 @@
 const fs = require('fs');
-var obj = fs.readFileSync('.serverless/cloudformation-template-update-stack.json', 'utf8');
+let crypto = require('crypto');
+
+var obj = fs.readFileSync('.serverless/cloudformation-template-update-stack.json', 'utf8').toString();
 let path = JSON.parse(obj).Resources.VerifyDashtokenLambdaFunction.Properties.Code.S3Key;
 fs.writeFileSync('path.txt',path.replace('/rest-api.zip',''));
+
+const fileHash = crypto.createHash('sha256');
+fileHash.setEncoding('base64');
+const readStream = fs.createReadStream('./.serverless/rest-api.zip');
+readStream.on('data', chunk => {
+    fileHash.write(chunk);
+}).on('end', () => {
+    fileHash.end();
+    let hash = fileHash.read();
+    console.log(hash);
+
+    let str = obj.replace(/("CodeSha256"\:\s?)(.*?)(\s*\})/g, `$1"${hash}"$3`);
+    fs.writeFileSync('.serverless/cloudformation-template-update-stack.json', str);
+}).on('error', error => {
+    console.log(error);
+});
+
+
+
+console.log()
