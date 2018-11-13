59d4ffb8b0509779d8b25e3231a2cf7e933a76a0
Rahul Pawar <rahul.pawar@globant.com>
2018-11-13 17:54:48 +0530

    environment base deployment

--- getPath.js
+++ getPath.js
@@ -1,20 +1,17 @@
 const fs = require('fs');
-let crypto = require('crypto');
-
+const crypto = require('crypto');
 var obj = fs.readFileSync('.serverless/cloudformation-template-update-stack.json', 'utf8').toString();
 let path = JSON.parse(obj).Resources.VerifyDashtokenLambdaFunction.Properties.Code.S3Key;
-fs.writeFileSync('path.txt',path.replace('/rest-api-dev.zip',''));
-
 const fileHash = crypto.createHash('sha256');
+const readStream = fs.createReadStream('./.serverless/rest-api.zip');
+
+fs.writeFileSync('path.txt', path.replace('/rest-api.zip', ''));
 fileHash.setEncoding('base64');
-const readStream = fs.createReadStream('./.serverless/rest-api-dev.zip');
 readStream.on('data', chunk => {
     fileHash.write(chunk);
 }).on('end', () => {
     fileHash.end();
     let hash = fileHash.read();
-    console.log(hash);
-
     let str = obj.replace(/("CodeSha256"\:\s?)(.*?)(\s*\})/g, `$1"${hash}"$3`);
     fs.writeFileSync('.serverless/cloudformation-template-update-stack.json', str);
 }).on('error', error => {
